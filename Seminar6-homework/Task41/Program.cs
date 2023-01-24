284c94544faf579dbe3c497aa91df54ea159c8cf
dzhukov <big_bug@list.ru>
2023-01-24 18:27:24 +0000

    Added homework for Seminar 6

---
+++ Seminar6-homework/Task41/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Сколько чисел планируется для ввода?");
+int m = int.Parse(ReadLine());
+int[] MyArray = new int[m];
+int CntPositiveNumbers = GetCntPositiveNumbers(MyArray);
+
+WriteLine($"Введенные числа: [{String.Join(",", MyArray)}]");
+WriteLine($"Количество положительных чисел: {CntPositiveNumbers}");
+
+int GetCntPositiveNumbers(int[] MyArray)
+{
+    int CntPositiveNumbers = 0;
+    for (int i = 0; i < MyArray.Length; i++)
+    {
+        MyArray[i] = int.Parse(ReadLine());
+        CntPositiveNumbers += MyArray[i] > 0 ? 1 : 0;
+
+    }
+    return CntPositiveNumbers;
+}
+
---
+++ Seminar6-homework/Task43/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+Clear();
+Write("Введите значение переменной b1: ");
+double b1 = int.Parse(ReadLine());
+WriteLine();
+Write("Введите значение переменной k1: ");
+double k1 = int.Parse(ReadLine());
+WriteLine();
+Write("Введите значение переменной b2: ");
+double b2 = int.Parse(ReadLine());
+WriteLine();
+Write("Введите значение переменной k2: ");
+double k2 = int.Parse(ReadLine());
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+WriteLine();
+WriteLine($"Координаты точки пересечения прямых: x={x}, y={y}");
+
